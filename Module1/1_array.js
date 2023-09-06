296d09cd622ab0ac6b3b533cc0f0526a828acb4b
anjalirao2001 <anjali07rao@gmail.com>
2023-09-06 23:09:21 +0530

    Basics Code for JavaScript

---
+++ Module1/1_array.js
@@ -0,0 +1,22 @@
+console.log("Hello from JavaScript")
+
+// ARRAY PROVIDES YOU AN ORDERED COLLECTION OF DATA
+ 
+var arr = [12,'Anjali', true, 12.4]
+console.log(arr)
+
+//ACCESSING THE ELEMENTS
+console.log('Accessing')
+var a = arr[2]
+
+console.log(a)
+
+//REPLACE THE ARRAY ELEMENTS USING INDEX
+console.log('Replacing')
+arr[1] = 'Shashi'
+
+console.log(arr)
+
+//FINDING LENGTH OF THE ARRAY
+
+console.log(`The length of the array is `+arr.length)
---
+++ Module1/Inbuilt_Method.js
@@ -0,0 +1,25 @@
+//INBUILT JS ARRAY METHOD
+var arr = [12 , 14 , 56 , 57]
+
+console.log(arr)
+
+//POP() METHOD - REMOVE AN ELEMENT FROM END OF THE ARRAY
+var c = arr.pop()
+console.log(`Poped element `+c)
+
+//PUSH() METHOD - ADD AN ELEMENT TO THE END OF THE ARRAY
+
+var c = arr.push(100)
+console.log(`Pushed element `+c)
+
+// SHIFT() METHOD - REMOVE FIRST ELEMENT FROM THE ARRAY 
+
+var c = arr.shift()
+console.log(`Shifted element `+c)
+console.log(arr)
+
+// UNSHIFT() METHOD - ADD AN ELEMENT AT THE STARTING OF THE ARRAY
+
+var c = arr.unshift(20)
+console.log(`Unshifted element `+c)
+console.log(arr)
